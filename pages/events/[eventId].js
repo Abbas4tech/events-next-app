a17409a10dfd810add415501768d7ab50c1e010f
Abbas4tech <shaikhabbas54@gmail.com>
2022-10-30 04:21:26 +0530

    Added event-detailed page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,8 +1,37 @@
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import EventContent from "../../components/event-detail/event-content";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventSummary from "../../components/event-detail/event-summary";
+
+import { getEventById } from "../../data/dummy-data";
+
 const EventDetailedPage = () => {
+  const router = useRouter();
+
+  const event = getEventById(router.query.eventId);
+
+  if (!event) {
+    return (
+      <Fragment>
+        <p>No Event Found!</p>
+      </Fragment>
+    );
+  }
+
   return (
-    <div>
-      <h1>Event Detailed Page</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   );
 };
 
